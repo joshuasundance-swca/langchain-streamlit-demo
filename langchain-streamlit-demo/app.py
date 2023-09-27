81efdb75ba49c5a36bd3f5db82abc063e92339fb
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-09-27 10:47:40 -0400

    bump version 0.0.2 -> 0.0.3

--- langchain-streamlit-demo/app.py
+++ langchain-streamlit-demo/app.py
@@ -26,7 +26,7 @@ from langchain_experimental.data_anonymizer import PresidioReversibleAnonymizer
 from langsmith.client import Client
 from streamlit_feedback import streamlit_feedback
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 # --- Initialization ---
 st.set_page_config(

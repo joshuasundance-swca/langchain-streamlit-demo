6a5206daa0170076d2e55f3878204571c03b0dbe
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-12-13 17:57:20 -0500

    bump version 2.0.1 -> 2.1.0

--- langchain-streamlit-demo/app.py
+++ langchain-streamlit-demo/app.py
@@ -31,7 +31,7 @@ from research_assistant.chain import get_chain as get_research_assistant_chain
 from python_coder import get_agent as get_python_agent
 
 
-__version__ = "2.0.1"
+__version__ = "2.1.0"
 
 # --- Initialization ---
 st.set_page_config(
